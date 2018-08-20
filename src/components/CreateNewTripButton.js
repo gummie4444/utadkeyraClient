0e009fba8263459ae553996d787bd6237ae5b47a
Guðmundur Egill <gudmunduregill@Gumundurs-MacBook-Pro.local>
2018-08-20 13:08:27 +0000

    make the plus white again

--- src/components/CreateNewTripButton.js
+++ src/components/CreateNewTripButton.js
@@ -11,7 +11,7 @@ export default ({ click }) => (
           width:75px;
           background:blue;
           border-radius:50%;
-          color:white;
+          color:white !important;
           background:#50E3C2;
           box-shadow:0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
           display: flex;
